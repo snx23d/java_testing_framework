45145703004706d3fe573fa5d85389986f8d9e7e
snx23d <antotalgin@gmail.com>
2023-12-12 10:11:12 +0100

    cleanup2

--- java_testing_framework/src/test/java/example/steps/context/SpringContext.java
+++
@@ -1,9 +0,0 @@
-package example.steps.context;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"example"})
-public class SpringContext {
-}
--- java_testing_framework/src/test/resources/features/example.feature
+++
@@ -1,7 +0,0 @@
-@complete
-Feature: Example feature
-
-
-  Scenario: Searching Google...
-    Given I search for "test automation"
-#    Then I google should show me results!
